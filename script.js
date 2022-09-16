fd9874d32f996fa3bb849af688fcb494ff80e15b
Taiwo Omoyeni <51166816+Shunlexxi@users.noreply.github.com>
2022-09-16 11:47:04 +0100

    Update and rename index.js to script.js

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
+const tabBtn = document.getElementById("tab-btn")
 
+
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+
+//LOCALSTORAGE
 //save a key-value pair in localStorage
 //localStorage.setItem("myLeads", "www.linkedin.com")
 
@@ -17,17 +23,44 @@ const ulEl = document.getElementById("ul-el")
 //clearing the localstorage
 //localStorage.clear()
 
+
+
+
 //get the leads from the localStorage & storing it in a var
 let savedLead = JSON.parse( localStorage.getItem("myLeads") )
-console.log(savedLead)
 
 
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
+
+
+tabBtn.addEventListener('click', function() {
+
+    //console.log(tabs[0].url)
+    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        console.log(tabs)
+
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("mylead", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+    
+    //COMPLETE TABS
+    // const tabs = [
+    //     {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+    // ]
+    //mySolution0
+    // localStorage.setItem("link", "https://www.linkedin.com/in/per-harald-borgen/")
+    // let linkFromLocalStorage = localStorage.getItem("link")
+    // render(linkFromLocalStorage)
+})
+
+
 
 
 inputBtn.addEventListener("click", function() {
@@ -36,24 +69,27 @@ inputBtn.addEventListener("click", function() {
     
     
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
 
-    console.log( localStorage.getItem("myLeads") )
 })
 
 
-deleteBtn.addEventListener('dblclick', function() {
-    //call clear
-    leadsFromLocalStorage = null
+deleteBtn.addEventListener("dblclick", function() {
+    //clear localStorage, myLeads & DOM
+    localStorage.clear()
+    myLeads = []
+    ulEl.textContent = ""
     console.log("cleared local storage")
 })
 
-    
-//listItems is later reassign to <li> and mylead[i] - via loop
 
-function renderLeads() {
+
+
+//making render re-usable; passing parameter
+function render(leads) {
+    
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         
         //Mine
         //listItems += "<li><a href=" + myLeads[i] + " target=" + "_blank" + ">" + myLeads[i] + "</a></li>"
@@ -64,14 +100,13 @@ function renderLeads() {
         //Template string
         listItems += `
         <li>
-            <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
+            <a target='_blank' href='${leads[i]}'>${leads[i]}</a>
         </li>
         `
-        console.log(listItems)
     }
 
 
-//<a href="https://www.facebook.com" target="_blank">Check</a>    
+    //<a href="https://www.facebook.com" target="_blank">Check</a>    
 
     //render the listItems inside the unordered list; ulEl.innerHTML
     ulEl.innerHTML = listItems
@@ -81,13 +116,10 @@ function renderLeads() {
 
  //rendering the leads
 //  for (let i = 0; i < myLeads.length; i++){
-        
-                
+//ulEl.innerHTML += "<li>" + "<a href=" + myLeads[i] + ">" + myLeads[i] + "</a></li>"
 
-    //ulEl.innerHTML += "<li>" + "<a href=" + myLeads[i] + ">" + myLeads[i] + "</a></li>"
-
-    //or using document.creteElement and .append
-    // const li = document.createElement("li")
-    // li.textContent = myLeads[i]
-    // ulEl.append(li)
-    //}
+//or using document.creteElement and .append
+// const li = document.createElement("li")
+// li.textContent = myLeads[i]
+// ulEl.append(li)
+//}
